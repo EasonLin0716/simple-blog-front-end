f0803c5a9adae37f3ba5687982f514acfa322cdd
Eason Lin <uu9924079@gmail.com>
2019-12-16 15:08:17 +0800

    chore: add SignIn, SignUp view and route

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,22 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
 import Posts from '../views/Posts.vue'
+import SignIn from '../views/SignIn.vue'
+import SignUp from '../views/SignUp.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/signin',
+    name: 'sign-in',
+    component: SignIn
+  },
+  {
+    path: '/signup',
+    name: 'sign-up',
+    component: SignUp
+  },
   {
     path: '/posts',
     name: 'posts',
