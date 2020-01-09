ee1dadc4aa9543274a6966003e73c963ddc3a85e
Eason Lin <uu9924079@gmail.com>
2020-01-09 22:05:05 +0800

    chore: index

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -1,7 +1,120 @@
 <template>
-  <div>
-    <h1>POSTS</h1>
-    <h2 v-for="post in posts" :key="post.id">{{ post.title }}</h2>
+  <div id="posts">
+    <div class="row" id="top-posts">
+      <div class="col-4 col-left">
+        <div id="col-left-box">
+          <router-link :to="'/posts/' + posts[0].id">
+            <img :src="posts[0].cover" alt="cover" />
+            <h2>{{ posts[0].title }}</h2>
+            <p>{{ posts[0].content.substring(0, 50) }}...</p>
+          </router-link>
+          <router-link :to="'/users/' + posts[0].authorId">
+            <span class="text-secondary d-block">{{ posts[0].author }}</span>
+          </router-link>
+          <span>{{ posts[0].monthDay }}&bull;{{ posts[0].readTime }}</span>
+        </div>
+      </div>
+      <div class="col-4 col-mid">
+        <div id="col-mid-box">
+          <div id="img-box">
+            <router-link :to="'/posts/' + posts[1].id">
+              <img :src="posts[1].cover" alt="cover" />
+            </router-link>
+          </div>
+          <div id="info">
+            <router-link :to="'/posts/' + posts[1].id">
+              <h6>{{ posts[1].title }}</h6>
+            </router-link>
+            <router-link :to="'/users/' + posts[1].authorId">
+              <span class="text-secondary d-block">{{ posts[1].author }}</span>
+            </router-link>
+            <span>{{ posts[1].monthDay }}&bull;{{ posts[1].readTime }}</span>
+          </div>
+        </div>
+        <div id="col-mid-box">
+          <div id="img-box">
+            <router-link :to="'/posts/' + posts[2].id">
+              <img :src="posts[2].cover" alt="cover" />
+            </router-link>
+          </div>
+          <div id="info">
+            <router-link :to="'/posts/' + posts[2].id">
+              <h6>{{ posts[2].title }}</h6>
+            </router-link>
+            <router-link :to="'/users/' + posts[2].authorId">
+              <span class="text-secondary d-block">{{ posts[2].author }}</span>
+            </router-link>
+            <span>{{ posts[2].monthDay }}&bull;{{ posts[2].readTime }}</span>
+          </div>
+        </div>
+        <div id="col-mid-box">
+          <div id="img-box">
+            <router-link :to="'/posts/' + posts[3].id">
+              <img :src="posts[3].cover" alt="cover" />
+            </router-link>
+          </div>
+          <div id="info">
+            <router-link :to="'/posts/' + posts[3].id">
+              <h6>{{ posts[3].title }}</h6>
+            </router-link>
+            <router-link :to="'/users/' + posts[3].authorId">
+              <span class="text-secondary d-block">{{ posts[3].author }}</span>
+            </router-link>
+            <span>{{ posts[3].monthDay }}&bull;{{ posts[3].readTime }}</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-4 col-right">
+        <div id="col-right-box">
+          <router-link :to="'/posts/' + posts[4].id">
+            <img :src="posts[4].cover" alt="cover" />
+            <h2>{{ posts[4].title }}</h2>
+            <p>{{ posts[4].content.substring(0, 50) }}...</p>
+          </router-link>
+          <router-link :to="'/users/' + posts[3].authorId">
+            <span class="text-secondary d-block">{{ posts[4].author }}</span>
+          </router-link>
+          <span>{{ posts[4].monthDay }}&bull;{{ posts[4].readTime }}</span>
+        </div>
+      </div>
+    </div>
+    <hr />
+    <div class="row" id="contents">
+      <div class="col-8">
+        <div v-for="post in posts" :key="post.id" id="post">
+          <div id="p-left">
+            <router-link :to="'/posts/' + post.id">
+              <h4>{{ post.title }}</h4>
+              <p>{{ post.content.substring(0, 40) }}...</p>
+            </router-link>
+            <div id="bottom">
+              <div>
+                <router-link :to="'/users/' + post.authorId">
+                  <span class="d-block">{{ post.author }}</span>
+                </router-link>
+                <span>{{ post.monthDay }}&bull;{{ post.readTime }}</span>
+              </div>
+              <div id="icons">
+                <font-awesome-icon
+                  @click="handleBookmark"
+                  :icon="['far', 'bookmark']"
+                  id="bookmark"
+                />
+                <img :src="ellipsis" alt="ellipsis" id="ellipsis" />
+              </div>
+            </div>
+          </div>
+          <div id="p-right">
+            <router-link :to="'/posts/' + post.id">
+              <img :src="post.cover" alt="cover" />
+            </router-link>
+          </div>
+        </div>
+      </div>
+      <div class="col-4">
+        <!-- Display info -->
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,10 +122,12 @@
 /* eslint-disable */
 import postAPI from './../apis/posts'
 export default {
+  name: 'Posts',
   components: {},
   data() {
     return {
-      posts: []
+      posts: [],
+      ellipsis: require('../../static/images/ellipsis-h-solid.svg')
     }
   },
   created() {
@@ -32,3 +147,78 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: #000;
+}
+
+#ellipsis,
+#bookmark {
+  width: 20px;
+  height: 20px;
+}
+
+#post {
+  display: flex;
+  justify-content: space-between;
+}
+
+img {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+#posts {
+  margin: 0 auto;
+  max-width: 1032px;
+}
+
+#col-left-box img,
+#col-right-box img {
+  height: 150px;
+  width: 100%;
+}
+
+#col-mid-box {
+  display: flex;
+  margin-bottom: 20px;
+}
+
+#img-box {
+  overflow: hidden;
+  height: 100px;
+  width: 100px;
+  margin-right: 10px;
+}
+
+#img-box img {
+  width: 100px;
+  height: 100px;
+}
+
+#bottom {
+  display: flex;
+  justify-content: space-between;
+}
+
+#bottom #icons {
+  display: flex;
+  align-items: center;
+}
+
+#p-left {
+  width: 100%;
+}
+
+#p-right {
+  width: 152px;
+  height: 122px;
+  margin-left: 40px;
+}
+
+#p-right img {
+  height: 100%;
+}
+</style>
