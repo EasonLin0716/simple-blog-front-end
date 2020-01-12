4ac7f158a334368efad96ef85ef0a4b3087e7865
Eason Lin <uu9924079@gmail.com>
2020-01-12 20:38:37 +0800

    chore: init  vue-quill-editor

--- src/main.js
+++ src/main.js
@@ -19,6 +19,11 @@ import store from './store'
 
 import '@/assets/global.css'
 
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 library.add(
   faSearch,
   faBell,
@@ -31,6 +36,7 @@ library.add(
 )
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.use(VueQuillEditor)
 
 Vue.config.productionTip = false
 
